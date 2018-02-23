d911bad0621714fd19431fe824ac473b3d612f22
nikolaygiman <gnl@MAIL.RU>
2018-02-23 23:40:37 +0300

    Add singleton trait

---
+++ core/ZXC/Patterns/Singleton.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ZXC\Patterns;
+
+trait Singleton
+{
+    static private $instance = null;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+
+    static public function getInstance()
+    {
+        return
+            self::$instance === null
+                ? self::$instance = new static()//new self()
+                : self::$instance;
+    }
+}
