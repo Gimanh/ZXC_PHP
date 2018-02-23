37ba92fe931fb0d56df6d01f69edc2629472c548
nikolaygiman <gnl@MAIL.RU>
2018-02-23 23:37:49 +0300

    Add class config with tests

---
+++ core/test/SessionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+$dir = __DIR__;
+$config = [];
+$file = $dir . '/../index.php';
+if (file_exists($file)) {
+    include $file;
+}
+
+class SessionTest extends TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testAllMethods()
+    {
+
+        $sessionInstance = \ZXC\Classes\Session::getInstance();
+        $sessionConfig = ['prefix' => 'zxc_', 'time' => 6200, 'path' => '/', 'domain' => 'example.com'];
+        $sessionInstance->initialize($sessionConfig);
+
+        $this->assertFalse($sessionInstance->set(null, null));
+
+        $this->assertSame($sessionInstance->set('testKey', 'testValue'), 'testValue');
+
+        $this->assertFalse($sessionInstance->get('zxc_'));
+
+        $this->assertSame($sessionInstance->get('testKey'), 'testValue');
+
+        $this->assertTrue($sessionInstance->delete('testKey'));
+
+        $this->assertFalse($sessionInstance->get('testKey'));
+
+        $this->assertSame($sessionInstance->set('testKey', 'testValue'), 'testValue');
+
+        $this->assertSame($_SESSION, ['zxc_' => ['testKey' => 'testValue']]);
+
+        $sessionInstance->clear();
+
+        $this->assertSame($_SESSION['zxc_'], []);
+
+        $sessionInstance->destroy();
+
+        $this->assertEmpty($_SESSION);
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        unset($sessionConfig['domain']);
+
+        $sessionInstance->initialize($sessionConfig);
+    }
+}
