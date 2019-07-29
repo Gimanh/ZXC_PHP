2e490c03d7eaba4e104ddb9c680feb3d28e8c0f8
nikolaygiman <gimanhead@gmail.com>
2019-07-29 22:08:48 +0300

    fix: tests

--- core/tests/ZXCTest.php
+++ core/tests/ZXCTest.php
@@ -29,32 +29,4 @@ class ZXCTest extends TestCase
         $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
         $this->assertSame(\ZXC\Native\Autoload::getAutoloadDirectories(), ['' => true, '../../' => true]);
     }
-
-    public function testGo()
-    {
-        $dir = __DIR__;
-        $config = require $dir . '/../config/config.php';
-        $zxc = \ZXC\ZXC::getInstance();
-
-        $_SERVER['HTTP_HOST'] = 'zxc:80';
-        $_SERVER['SERVER_NAME'] = 'zxcserver';
-        $_SERVER['SERVER_PORT'] = '80';
-        $_SERVER['REQUEST_METHOD'] = 'POST';
-        $_SERVER['REQUEST_URI'] = '/';
-        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-
-        $zxc->initialize($config);
-        $zxc->go();
-    }
-
-//    public static function tearDownAfterClass()
-//    {
-//        $logger = ModulesManager::getNewModule('Logger');
-//        $logger->setLogsFolder(Config::get('ZXC/Modules/Logger/options/folder'));
-//        $logger->setLogFileName(ZXC\ZXC::getInstance()->getLogFileName());
-//        unlink($logger->getFullLogFilePath());
-//        $logger->setLogsFolder(Config::get('ZXC/Modules/Logger/options/folder'));
-//        $logger->setLogFileName(Config::get('ZXC/Modules/Logger/options/file'));
-//        unlink($logger->getFullLogFilePath());
-//    }
 }
