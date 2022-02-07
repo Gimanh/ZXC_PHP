964d6ce5ae214e06ab4bbd26eb959a90f63cf4a3
nikolaygiman <gimanhead@gmail.com>
2022-02-08 00:39:35 +0300

    wip: mail

--- src/ZXC/Modules/Mailer/Mail.php
+++ src/ZXC/Modules/Mailer/Mail.php
@@ -1,36 +1,65 @@
 <?php
 
-
 namespace ZXC\Modules\Mailer;
 
-
 use ZXC\Traits\Module;
+use ZXC\Native\Modules;
 use ZXC\Interfaces\IModule;
 use InvalidArgumentException;
+use ZXC\Modules\Mailer\Tx\SMTP;
 use ZXC\Modules\Mailer\Tx\Mailer;
+use ZXC\Interfaces\Psr\Log\LoggerInterface;
 
 
 class Mail extends Mailer implements IModule
 {
     use Module;
 
-    protected $version = '0.0.1';
-    /**
-     * Initialize class with config
-     * @param array $config
-     * @return bool
-     */
-    public function init(array $config = null)
+    protected string $host;
+
+    protected int $port;
+
+    protected string $username;
+
+    protected string $password;
+
+    protected ?string $encryption;
+
+    protected string $fromName;
+
+    protected string $fromEmail;
+
+    public function __construct(LoggerInterface $logger = null)
     {
-        if (empty($config['server']) || empty($config['port']) ||
-            empty($config['user']) || empty($config['password']) ||
-            empty($config['from']) || empty($config['fromEmail'])) {
-            throw new InvalidArgumentException('Incorrect config for SMTP');
+        $loggerModule = Modules::get('logger');
+        if ($loggerModule instanceof LoggerInterface) {
+            $logger = $loggerModule;
         }
-        $secure = isset($config['ssl']) ? $config['ssl'] : null;
-        $this->setServer($config['server'], $config['port'], $secure)
-            ->setAuth($config['user'], $config['password'])
-            ->setFrom($config['from'], $config['fromEmail']);
-        return true;
+        parent::__construct($logger);
+    }
+
+    public function init(array $options = [])
+    {
+        $requiredConfig = ["host", "port", "username", "password", "fromName", "fromEmail"];
+        foreach ($requiredConfig as $item) {
+            if (!isset($options[$item])) {
+                throw new InvalidArgumentException("Incorrect config for SMTP. Key $item is required.");
+            }
+        }
+        $this->host = $options['host'];
+        $this->port = $options['port'];
+        $this->username = $options['username'];
+        $this->password = $options['password'];
+        $this->encryption = $options['encryption'] ?? null;
+        $this->fromName = $options['fromName'];
+        $this->fromEmail = $options['fromEmail'];
+        $this->setServer($this->host, $this->port, $this->encryption)
+            ->setAuth($this->username, $this->password)
+            ->setFrom($this->fromName, $this->fromEmail);
+    }
+
+    public function getSmtp(): SMTP
+    {
+        return $this->smtp;
     }
 }
